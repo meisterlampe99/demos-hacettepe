b6bb714adb97c932f54fc990ec48675674192848
Patrick Lampl <patrick.lampl@fh-joanneum.at>
2026-05-12 22:00:20 +0200

    added examples for SiPEED Tang Nano 1k, Gowin FPGA

---
+++ demo_gowin/02-ORG-LED-Blink-SV2/led.sv
@@ -0,0 +1,23 @@
+module led (
+    input sys_clk,
+    input sys_rst_n,
+    output logic [2:0] led // 110 R, 101 B, 011 G
+);
+
+logic [31:0] counter;
+
+always_ff @(posedge sys_clk or negedge sys_rst_n) begin
+    if (!sys_rst_n) begin
+        counter <= 31'd0;
+        led <= 3'b110;
+    end
+    else if (counter < 31'd1350_0000)       // 0.5s delay
+        counter <= counter + 1;
+    else begin
+        counter <= 31'd0;
+        led[2:0] <= {led[1:0],led[2]};
+    end
+end
+
+endmodule
+
